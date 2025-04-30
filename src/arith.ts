7b5aac72b1c7471ec463ef08d718ecb219ae421c
Kazumasa Hirata <k_hirata@openwork.co.jp>
2025-05-01 01:08:00 +0900

    feat(arith): allow any type for if condition

--- src/arith.ts
+++ src/arith.ts
@@ -43,6 +43,38 @@ export function typecheckArith(t: Term): Type {
 	}
 }
 
+export function typecheckArith2(t: Term): Type {
+	switch (t.tag) {
+		case "true":
+			return { tag: "Boolean" };
+		case "false":
+			return { tag: "Boolean" };
+		case "if": {
+			typecheckArith(t.cond);
+			const [thnTy, elsTy] = [typecheckArith(t.thn), typecheckArith(t.els)];
+			if (thnTy.tag !== elsTy.tag) {
+				throw "branches must have the same type";
+			}
+			return thnTy;
+		}
+		case "number":
+			return { tag: "Number" };
+		case "add": {
+			const leftTy = typecheckArith(t.left);
+			if (leftTy.tag !== "Number") {
+				throw "number expected on left side of `+`";
+			}
+			const rightTy = typecheckArith(t.right);
+			if (rightTy.tag !== "Number") {
+				throw "number expected on right side of `+`";
+			}
+			return { tag: "Number" };
+		}
+		default:
+			throw `unknown term: ${JSON.stringify(t)}`;
+	}
+}
+
 if (import.meta.vitest) {
 	const { describe, expect, test } = await import("vitest");
 	const { parseArith } = await import("tiny-ts-parser");
@@ -54,6 +86,7 @@ if (import.meta.vitest) {
 			["1", { tag: "Number" }],
 			["1 + 2", { tag: "Number" }],
 			["true ? 1 : 0", { tag: "Number" }],
+			["true ? true : false", { tag: "Boolean" }],
 		])("OK: `%s`", (term, expected) => {
 			expect(typecheckArith(parseArith(term))).toEqual(expected);
 		});
@@ -65,6 +98,20 @@ if (import.meta.vitest) {
 			["1 + true", "number expected on right side of `+`"],
 		])("NG: `%s`", (term, expected) => {
 			expect(() => typecheckArith(parseArith(term))).toThrow(expected);
+		});
+	});
+
+	describe(typecheckArith2, () => {
+		test.each([
+			["true", { tag: "Boolean" }],
+			["false", { tag: "Boolean" }],
+			["1", { tag: "Number" }],
+			["1 + 2", { tag: "Number" }],
+			["true ? 1 : 0", { tag: "Number" }],
+			["true ? true : false", { tag: "Boolean" }],
+			["1 ? 1 : 0", { tag: "Number" }],
+		])("OK: `%s`", (term, expected) => {
+			expect(typecheckArith2(parseArith(term))).toEqual(expected);
 		});
 	});
 }

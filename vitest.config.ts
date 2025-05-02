89f325ce659ff8d0ab6aec1b6907de680f5df677
Kazumasa Hirata <50480499+kzmshx@users.noreply.github.com>
2025-05-03 01:35:50 +0900

    ci: Add `Check` and `Test` workflows (#1)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		includeSource: ["src/**/*.ts"],
+		coverage: {
+			reporter: ["text", "json-summary", "json"],
+		},
 	},
 	define: {
 		"import.meta.vitest": "undefined",

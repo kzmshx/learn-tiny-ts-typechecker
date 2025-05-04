523c8b0e358e78d9e23f775ac25a82cc93a33051
Kazumasa Hirata <k_hirata@openwork.co.jp>
2025-05-05 00:39:27 +0900

    feat(obj): support object type in `typeEq`

--- src/obj.ts
+++ src/obj.ts
@@ -10,7 +10,7 @@
  * - オブジェクトプロパティアクセスにおいて、オブジェクトにそのプロパティが存在すること
  */
 
-import { assert, ensure } from "./util.ts";
+import { assert, assertNever, ensure } from "./util.ts";
 
 type Term =
 	| { tag: "true" }
@@ -19,24 +19,32 @@ type Term =
 	| { tag: "number"; n: number }
 	| { tag: "add"; left: Term; right: Term }
 	| { tag: "var"; name: string }
-	| { tag: "func"; params: Param[]; body: Term }
+	| { tag: "func"; params: ParamType[]; body: Term }
 	| { tag: "call"; func: Term; args: Term[] }
 	| { tag: "seq"; body: Term; rest: Term }
 	| { tag: "const"; name: string; init: Term; rest: Term }
-	| { tag: "seq2"; body: Term[] }
-	| { tag: "const2"; name: string; init: Term };
+	| { tag: "objectNew"; props: PropTerm[] }
+	| { tag: "objectGet"; obj: Term; propName: string };
+
+type PropTerm = { name: string; term: Term };
 
 type Type =
 	| { tag: "Boolean" }
 	| { tag: "Number" }
-	| { tag: "Func"; params: Param[]; retType: Type };
+	| { tag: "Func"; params: ParamType[]; retType: Type }
+	| { tag: "Object"; props: PropType[] };
 
-type Param = { name: string; type: Type };
+type ParamType = { name: string; type: Type };
+
+type PropType = { name: string; type: Type };
 
 type TypeEnv = Record<string, Type>;
 
 function typeEq(a: Type, b: Type): boolean {
 	switch (b.tag) {
+		case "Number":
+		case "Boolean":
+			return a.tag === b.tag;
 		case "Func":
 			return (
 				a.tag === "Func" &&
@@ -44,8 +52,19 @@ function typeEq(a: Type, b: Type): boolean {
 				a.params.every((p, i) => typeEq(p.type, ensure(b.params[i]).type)) &&
 				typeEq(a.retType, b.retType)
 			);
+		case "Object": {
+			const bTypes = Object.fromEntries(b.props.map((p) => [p.name, p.type]));
+			return (
+				a.tag === "Object" &&
+				a.props.length === b.props.length &&
+				a.props.every((p) => {
+					const bType = bTypes[p.name];
+					return bType && typeEq(p.type, bType);
+				})
+			);
+		}
 		default:
-			return a.tag === b.tag;
+			assertNever(b);
 	}
 }
 
@@ -117,23 +136,6 @@ export function typecheck(t: Term, tyEnv: TypeEnv): Type {
 			const initTy = typecheck(t.init, tyEnv);
 			return typecheck(t.rest, { ...tyEnv, [t.name]: initTy });
 		}
-		case "seq2": {
-			let lastTy: Type | null = null;
-			for (const term of t.body) {
-				if (term.tag === "const2") {
-					const initTy = typecheck(term.init, tyEnv);
-					tyEnv[term.name] = initTy;
-					lastTy = initTy;
-				} else {
-					lastTy = typecheck(term, tyEnv);
-				}
-			}
-			assert(lastTy !== null, "empty sequence", t);
-			return lastTy;
-		}
-		case "const2": {
-			throw "const2 must not be reached";
-		}
 		default:
 			throw `unknown term: ${JSON.stringify(t)}`;
 	}
@@ -159,15 +161,22 @@ export function typeShow(t: Type): string {
 
 if (import.meta.vitest) {
 	const { describe, expect, test } = await import("vitest");
-	const { parseBasic, parseBasic2 } = await import("tiny-ts-parser");
+	const { parseObj } = await import("tiny-ts-parser");
+
+	const trim = (s: string): string => s.trim().replace(/\s+/g, " ");
 
 	const bool = (): Type => ({ tag: "Boolean" });
 	const num = (): Type => ({ tag: "Number" });
-	const param = (name: string, type: Type): Param => ({ name, type });
+	const param = (name: string, type: Type): ParamType => ({ name, type });
+	const prop = (name: string, type: Type): PropType => ({ name, type });
 	const fn = (params: [name: string, type: Type][], retType: Type): Type => ({
 		tag: "Func",
 		params: params.map(([name, type]) => param(name, type)),
 		retType,
+	});
+	const obj = (props: [name: string, type: Type][]): Type => ({
+		tag: "Object",
+		props: props.map(([name, type]) => prop(name, type)),
 	});
 
 	describe(typecheck, () => {
@@ -187,23 +196,18 @@ if (import.meta.vitest) {
 			["const x = 1", num()],
 			["const x = 1; const x = true", bool()],
 			[
-				`
-				const add = (x: number, y: number) => {
+				trim(`const add = (x: number, y: number) => {
 					const z = x + y;
 					return z;
 				};
 				const select = (cond: boolean, thn: number, els: number) => cond ? thn : els;
 				const x = add(1, add(2, 3));
 				const y = select(true, x, 0);
-				y;
-				`
-					.trim()
-					.replace(/\s+/g, " "),
+							y;`),
 				num(),
 			],
 		])("OK: `%s`", (term, expected) => {
-			expect(typecheck(parseBasic(term), {})).toStrictEqual(expected);
-			expect(typecheck(parseBasic2(term), {})).toStrictEqual(expected);
+			expect(typecheck(parseObj(term), {})).toStrictEqual(expected);
 		});
 
 		test.each([
@@ -218,20 +222,15 @@ if (import.meta.vitest) {
 			["true + 1; 1", "number expected on left side of `+`"],
 			["1; true + 1", "number expected on left side of `+`"],
 			[
-				`
-				const add = (x: number, y: number) => {
+				trim(`const add = (x: number, y: number) => {
 					const z = x + y;
 					return z;
 				};
-				z;
-				`
-					.trim()
-					.replace(/\s+/g, " "),
+							z;`),
 				"unknown variable: z",
 			],
 		])("NG: `%s`", (term, expected) => {
-			expect(() => typecheck(parseBasic(term), {})).toThrow(expected);
-			expect(() => typecheck(parseBasic2(term), {})).toThrow(expected);
+			expect(() => typecheck(parseObj(term), {})).toThrow(expected);
 		});
 	});
 
@@ -242,6 +241,28 @@ if (import.meta.vitest) {
 			[fn([], num()), fn([], num())],
 			[fn([["x", bool()]], num()), fn([["x", bool()]], num())],
 			[fn([["x", bool()]], num()), fn([["y", bool()]], num())],
+			// オブジェクト
+			[obj([["a", num()]]), obj([["a", num()]])],
+			[
+				obj([
+					["a", num()],
+					["b", bool()],
+				]),
+				obj([
+					["a", num()],
+					["b", bool()],
+				]),
+			],
+			[
+				obj([
+					["a", num()],
+					["b", bool()],
+				]),
+				obj([
+					["b", bool()],
+					["a", num()],
+				]),
+			],
 		])("`%s` == `%s`", (a, b) => {
 			expect(typeEq(a, b)).toStrictEqual(true);
 		});
@@ -253,6 +274,15 @@ if (import.meta.vitest) {
 			[fn([], num()), fn([], bool())],
 			[fn([], num()), fn([["x", num()]], num())],
 			[fn([["x", num()]], num()), fn([["x", bool()]], num())],
+			[obj([["a", num()]]), obj([["a", bool()]])],
+			[obj([["a", num()]]), obj([["b", num()]])],
+			[
+				obj([["a", num()]]),
+				obj([
+					["a", num()],
+					["b", bool()],
+				]),
+			],
 		])("`%s` != `%s`", (a, b) => {
 			expect(typeEq(a, b)).toStrictEqual(false);
 		});

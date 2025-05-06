c5e2358466ebb78f0a4c8b36baf9d491a85ca1e6
Kazumasa Hirata <50480499+kzmshx@users.noreply.github.com>
2025-05-07 00:00:46 +0900

    feat(tiny-ts-typechecker): add `sub` mode (#10)

--- bin/tiny-ts-typechecker.ts
+++ bin/tiny-ts-typechecker.ts
@@ -1,11 +1,18 @@
 #!/usr/bin/env -S node --experimental-strip-types
 
 import { parseArgs } from "node:util";
-import { parseArith, parseBasic, parseObj, parseRecFunc } from "tiny-ts-parser";
+import {
+	parseArith,
+	parseBasic,
+	parseObj,
+	parseRecFunc,
+	parseSub,
+} from "tiny-ts-parser";
 import { typecheck as typecheckArith } from "../src/arith.ts";
 import { typecheck as typecheckBasic } from "../src/basic.ts";
 import { typecheck as typecheckObj } from "../src/obj.ts";
 import { typecheck as typecheckRecFunc } from "../src/rec-func.ts";
+import { typecheck as typecheckSub } from "../src/sub.ts";
 
 const { values, positionals } = parseArgs({
 	args: process.argv.slice(2),
@@ -33,6 +40,7 @@ if (positionals.length === 0 || positionals[0] === undefined) {
 			"  tiny-ts-typechecker 'const x = 1; const y = 2; x + y' --mode basic",
 			"  tiny-ts-typechecker 'const obj = { a: 1, b: true }; obj.a' --mode obj",
 			"  tiny-ts-typechecker 'const f = (x: number): number => f(x)' --mode rec-func",
+			"  tiny-ts-typechecker 'const f = (x: { a: number, b: number }): number => x.a + x.b; f({ a: 1, b: 2, c: 3 });' --mode sub",
 		].join("\n"),
 	);
 	process.exit(1);
@@ -52,6 +60,9 @@ switch (values.mode) {
 		break;
 	case "rec-func":
 		console.dir(typecheckRecFunc(parseRecFunc(content), {}));
+		break;
+	case "sub":
+		console.dir(typecheckSub(parseSub(content), {}));
 		break;
 	default:
 		console.error(`Unknown mode: ${values.mode}`);
